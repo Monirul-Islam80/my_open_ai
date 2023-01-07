2dbc12293c82c8965f0a19fd74987cf9a8179af2
Monirul Islam <monirulislam49007@gmail.com>
2023-01-08 00:33:03 +0600

    new api url

--- clinet/script.js
+++ clinet/script.js
@@ -63,7 +63,7 @@ const handleSubmit = async (e) => {
 
     loading(mesDiv);
 
-    const response = await fetch("http://localhost:5000/", {
+    const response = await fetch("https://monirsai.onrender.com", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
