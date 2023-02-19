3a75745864159499b519b1a6a52f985f0a17deff
Monirul Islam <monirulislam49007@gmail.com>
2023-02-19 21:30:40 +0600

    fixing frontend for mobile 2

--- clinet/script.js
+++ clinet/script.js
@@ -5,7 +5,27 @@ const chatContainer = document.querySelector("#chat_container");
 const btn = document.querySelector("#subbtn");
 const textbox = document.querySelector("#textbox");
 const info = document.querySelector(".info")
-let loadingAnimation;
+let loadingAnimation;// define a function that sets min-height of my-element to window.innerHeight:
+
+const setHeight = () => {
+    document.getElementById("app").style.minHeight = window.innerHeight + "px"
+};
+
+// define mobile screen size:
+
+let deviceWidth = window.matchMedia("(max-width: 1024px)");
+
+if (deviceWidth.matches) {
+    // set an event listener that detects when innerHeight changes:
+
+    window.addEventListener("resize", setHeight);
+
+    // call the function once to set initial height:
+
+    setHeight();
+}
+
+
 btn.addEventListener("click", () => info.style.display = "none");
 textbox.addEventListener("keypress", (e) => {
     if (event.keyCode == 13 && textbox.value == '') {
